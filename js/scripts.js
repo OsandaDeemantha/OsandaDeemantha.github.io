1017af5f55e61a673c34514b6fb6ca18de9138c0
osandadeemantha <osanda.16@cse.mrt.ac.lk>
2020-12-22 02:46:06 +0530

    customize map

--- js/scripts.js
+++ js/scripts.js
@@ -467,7 +467,7 @@ $(function () {
 */
 
 function initMap() {
-	var myLatlng = new google.maps.LatLng(40.773328,-73.960088); // <- Your latitude and longitude
+	var myLatlng = new google.maps.LatLng(7.475463,79.854644); // <- Your latitude and longitude
 	var styles = [
 	{
 		"featureType": "water",
@@ -484,15 +484,95 @@ function initMap() {
 			"color": "#eeeeee"
 		}]
 	}]
+	var stylesDark = [
+		{ elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+		{ elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+		{ elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+		{
+		  featureType: "administrative.locality",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#d59563" }],
+		},
+		{
+		  featureType: "poi",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#d59563" }],
+		},
+		{
+		  featureType: "poi.park",
+		  elementType: "geometry",
+		  stylers: [{ color: "#263c3f" }],
+		},
+		{
+		  featureType: "poi.park",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#6b9a76" }],
+		},
+		{
+		  featureType: "road",
+		  elementType: "geometry",
+		  stylers: [{ color: "#38414e" }],
+		},
+		{
+		  featureType: "road",
+		  elementType: "geometry.stroke",
+		  stylers: [{ color: "#212a37" }],
+		},
+		{
+		  featureType: "road",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#9ca5b3" }],
+		},
+		{
+		  featureType: "road.highway",
+		  elementType: "geometry",
+		  stylers: [{ color: "#746855" }],
+		},
+		{
+		  featureType: "road.highway",
+		  elementType: "geometry.stroke",
+		  stylers: [{ color: "#1f2835" }],
+		},
+		{
+		  featureType: "road.highway",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#f3d19c" }],
+		},
+		{
+		  featureType: "transit",
+		  elementType: "geometry",
+		  stylers: [{ color: "#2f3948" }],
+		},
+		{
+		  featureType: "transit.station",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#d59563" }],
+		},
+		{
+		  featureType: "water",
+		  elementType: "geometry",
+		  stylers: [{ color: "#17263c" }],
+		},
+		{
+		  featureType: "water",
+		  elementType: "labels.text.fill",
+		  stylers: [{ color: "#515c6d" }],
+		},
+		{
+		  featureType: "water",
+		  elementType: "labels.text.stroke",
+		  stylers: [{ color: "#17263c" }],
+		},
+	  ]
 
 	var mapOptions = {
-		zoom: 14,
+		zoom: 10,
 		center: myLatlng,
-		mapTypeControl: false,
+		mapTypeControl: true,
 		disableDefaultUI: true,
 		zoomControl: true,
-		scrollwheel: false,
-		styles: styles
+		scrollwheel: false
+		// styles: stylesDark
 	}
 	
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
